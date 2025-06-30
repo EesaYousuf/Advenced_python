82628114ef530b373ee44ea050307f391f622180
eesa <eesayousufyousuf@gmail.com>
2025-06-30 09:32:00 +0530

    try

--- day20.py
+++ day20.py
@@ -2,4 +2,10 @@ import logging
 import time
 import random
 from typing import Callable, TypeVar
+# Set up structured logging
+logging.basicConfig(level=logging.INFO)
+logger = logging.getLogger(__name__)
+
+T = TypeVar("T")
+
 
