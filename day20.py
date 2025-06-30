91324c9837eed2b246f913d70ab9506f743688ca
eesa <eesayousufyousuf@gmail.com>
2025-06-30 09:33:19 +0530

    try1

--- day20.py
+++ day20.py
@@ -7,5 +7,14 @@ logging.basicConfig(level=logging.INFO)
 logger = logging.getLogger(__name__)
 
 T = TypeVar("T")
+# Custom exceptions
+class APIError(Exception):
+    """Raised when the API call fails"""
+    pass
+
+class NetworkError(APIError):
+    """Specific network-related failure"""
+    pass
+
 
 
