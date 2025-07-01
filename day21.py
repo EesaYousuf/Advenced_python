facbfa8d48d4873bd83f2f04c9c6d6ee169cc901
EesaYousuf <eesayousufyousuf@gmail.com>
2025-07-01 21:04:26 +0530

    try2

--- day21.py
+++ day21.py
@@ -6,3 +6,9 @@ import webbrowser
 import os
 import threading
 import time
+class LocationTracker:
+    def __init__(self, update_interval=10):
+        self.update_interval = update_interval
+        self.running = False
+        self.map_file = "live_location.html"
+
