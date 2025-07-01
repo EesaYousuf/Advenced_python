c6b388112382e7cc218158565699641d1fc1351f
EesaYousuf <eesayousufyousuf@gmail.com>
2025-07-01 21:12:50 +0530

    try6

--- day22.py
+++ day22.py
@@ -29,3 +29,16 @@ def sigmoid(x):
 def softmax(x):
     exps = np.exp(x - np.max(x, axis=1, keepdims=True))
     return exps / np.sum(exps, axis=1, keepdims=True)
+# Loss
+def cross_entropy(pred, true):
+    return -np.sum(true * np.log(pred + 1e-9)) / pred.shape[0]
+
+# Training
+def train(epochs=1000, lr=0.1):
+    global W1, b1, W2, b2
+    for epoch in range(epochs):
+        # Forward
+        z1 = X_train @ W1 + b1
+        a1 = sigmoid(z1)
+        z2 = a1 @ W2 + b2
+        a2 = softmax(z2)
