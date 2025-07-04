6ab8598baa0804cb86600437f8ca8c380508458d
Eesa yousuf <eesayousufyousuf@gmail.com>
2025-07-04 15:56:24 +0530

    try2

--- day25.py
+++ day25.py
@@ -10,3 +10,9 @@ points = "pts_in_hull.npy"
 
 net = cv2.dnn.readNetFromCaffe(prototxt, model)
 pts = np.load(points)
+# Add cluster centers to the model
+class8 = net.getLayerId("class8_ab")
+conv8 = net.getLayerId("conv8_313_rh")
+pts = pts.transpose().reshape(2, 313, 1, 1)
+net.getLayer(class8).blobs = [pts.astype(np.float32)]
+net.getLayer(conv8).blobs = [np.full([1, 313], 2.606, dtype="float32")]
