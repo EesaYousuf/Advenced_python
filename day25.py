1ee4803283d9d9081b769d1575035fdd4e013515
Eesa yousuf <eesayousufyousuf@gmail.com>
2025-07-04 15:57:28 +0530

    try3

--- day25.py
+++ day25.py
@@ -16,3 +16,20 @@ conv8 = net.getLayerId("conv8_313_rh")
 pts = pts.transpose().reshape(2, 313, 1, 1)
 net.getLayer(class8).blobs = [pts.astype(np.float32)]
 net.getLayer(conv8).blobs = [np.full([1, 313], 2.606, dtype="float32")]
+def colorize_image(img_path):
+    bw = cv2.imread(img_path)
+    normalized = bw.astype("float32") / 255.0
+    lab = cv2.cvtColor(normalized, cv2.COLOR_BGR2LAB)
+
+    resized = cv2.resize(lab, (224, 224))
+    L = resized[:, :, 0]
+    L -= 50
+
+    net.setInput(cv2.dnn.blobFromImage(L))
+    ab = net.forward()[0, :, :, :].transpose((1, 2, 0))
+    ab = cv2.resize(ab, (bw.shape[1], bw.shape[0]))
+
+    L = lab[:, :, 0]
+    colorized = np.concatenate((L[:, :, np.newaxis], ab), axis=2)
+    colorized = cv2.cvtColor(colorized, cv2.COLOR_LAB2BGR)
+    colorized = np.clip(colorized, 0, 1)
